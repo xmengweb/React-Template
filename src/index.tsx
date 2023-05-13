4519579cf04ff57a511c2f4bf98581492881a37f
xmengweb <dd2419247021@163.com>
2023-05-13 20:34:23 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "normalize.css";
-import "@/public/css/global.less";
+import "@/assets/css/global.less";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
